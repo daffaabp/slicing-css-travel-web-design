bcb87b0e9345862e05b038377ac4da10ae1869fc
daffaabp <daffa.budi2003@gmail.com>
2024-02-29 13:35:36 +0700

    finish section #produk

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,27 @@ let menuWrapper = document.querySelector('.menu-wrapper');
 menuOpen.addEventListener('click', function() {
    menuOpen.classList.toggle('bx-x');
    menuWrapper.classList.toggle('active');
-})
+});
+
+var swiper = new Swiper(".mySwiper", {
+   effect: "coverflow",
+   grabCursor: true,
+   centeredSlides: false,
+   spaceBetween: 30,
+   slidesPerView: "auto",
+   autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+   coverflowEffect: {
+     rotate: 0,
+     stretch: 0,
+     depth: 0,
+     modifier: 1,
+     slideShadows: true,
+   },
+   navigation: {
+      nextEl: ".bxs-chevron-right-circle",
+      prevEl: ".bx-chevron-left-circle",
+    },
+ });
