9dd58fc596129799f10327f950479baec8608844
daffaabp <daffa.budi2003@gmail.com>
2024-02-29 23:02:46 +0700

    finish section #banner2 dan footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,13 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".bx-chevron-left-circle",
     },
  });
+
+//  var swiper = new Swiper(".mySwiperTesti", {
+//    pagination: {
+//      el: ".swiper-pagination",
+//    },
+//    autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//  });
